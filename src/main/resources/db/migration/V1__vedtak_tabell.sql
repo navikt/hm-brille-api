8c6b4492f3342dfdb85181a83e5812d6e9f46372
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-06-21 15:19:33 +0200

    Added orgnr field to vedtak-table

--- src/main/resources/db/migration/V1__vedtak_tabell.sql
+++ src/main/resources/db/migration/V1__vedtak_tabell.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS vedtak (
     id                      UUID            NOT NULL PRIMARY KEY,
     fnr_bruker              varchar(11)     NOT NULL,
     fnr_innsender           varchar(11)     NOT NULL,
+    orgnr                   varchar(9)      NOT NULL,
     data                    JSONB           NOT NULL,
     opprettet               timestamp       NOT NULL DEFAULT (NOW())
 );
