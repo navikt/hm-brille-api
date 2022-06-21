ee2f3983a497403ff9dd6b6073c944a0ceb84fa3
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-06-21 14:50:51 +0200

    Indexes for likely lookups

--- src/main/resources/db/migration/V1__vedtak_tabell.sql
+++ src/main/resources/db/migration/V1__vedtak_tabell.sql
@@ -6,4 +6,7 @@ CREATE TABLE IF NOT EXISTS vedtak (
     opprettet               timestamp       NOT NULL DEFAULT (NOW())
 );
 
+CREATE INDEX vedtak_fnr_bruker ON vedtak (fnr_bruker, opprettet DESC);
+CREATE INDEX vedtak_fnr_innsender ON vedtak (fnr_innsender, opprettet DESC);
+
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
