8b1151dc7166d91b1f86348a4f1b318a609a758a
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-06-21 15:21:01 +0200

    Fixed length char

--- src/main/resources/db/migration/V1__vedtak_tabell.sql
+++ src/main/resources/db/migration/V1__vedtak_tabell.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS vedtak (
     id                      UUID            NOT NULL PRIMARY KEY,
-    fnr_bruker              varchar(11)     NOT NULL,
-    fnr_innsender           varchar(11)     NOT NULL,
-    orgnr                   varchar(9)      NOT NULL,
+    fnr_bruker              char(11)        NOT NULL,
+    fnr_innsender           char(11)        NOT NULL,
+    orgnr                   char(9)         NOT NULL,
     data                    JSONB           NOT NULL,
     opprettet               timestamp       NOT NULL DEFAULT (NOW())
 );
