abdbb678c01eeff34bb7823a28dbbecb366ebe5f
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-06-27 10:41:16 +0200

    Revert to pid for default userclaim field name

--- src/main/kotlin/no/nav/hjelpemidler/brille/Configuration.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/Configuration.kt
@@ -16,7 +16,7 @@ object Configuration {
             "altinn.serviceCode" to "5614",
             "altinn.serviceEditionCode" to "1",
             "digihot-oppslag.oppslagUrl" to "http://digihot-oppslag/api",
-            "userclaim" to "sub",
+            "userclaim" to "pid",
             "enhetsregisteret_base_url" to "https://data.brreg.no/enhetsregisteret/api"
         )
     )
