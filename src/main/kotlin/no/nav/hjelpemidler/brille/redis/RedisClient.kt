6981ac2cf13557e2d4f6e49c6576c32c57144e8a
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2022-06-30 13:40:54 +0200

    legg til redis-secret

--- src/main/kotlin/no/nav/hjelpemidler/brille/redis/RedisClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/redis/RedisClient.kt
@@ -2,12 +2,19 @@ package no.nav.hjelpemidler.brille.redis
 
 import mu.KotlinLogging
 import no.nav.hjelpemidler.brille.Configuration
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig
 import redis.clients.jedis.JedisPooled
 
 private val log = KotlinLogging.logger {}
 
 class RedisClient(val redisProps: Configuration.RedisProperties = Configuration.redisProperties) {
-    val jedis = JedisPooled(redisProps.host, redisProps.port)
+    val jedis = JedisPooled(
+        GenericObjectPoolConfig(),
+        redisProps.host,
+        redisProps.port,
+        2 * 1000, // 2 seconds
+        redisProps.password
+    )
 
     fun erOptiker(fnr: String): Boolean? {
         val erOptiker = jedis.get("fnr:$fnr:hpr:er.optiker")?.toBoolean()
