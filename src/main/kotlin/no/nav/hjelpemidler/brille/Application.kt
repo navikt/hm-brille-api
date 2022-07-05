356f5b6722067e42587fe1b47637065accdbf96f
matsbyfl <mats@byfuglien.net>
2022-07-05 15:14:44 +0200

    Oppslag på orghenet for alle tidligere brukte orgnummer for å få litt rikere api response

--- src/main/kotlin/no/nav/hjelpemidler/brille/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/Application.kt
@@ -27,6 +27,7 @@ import no.nav.hjelpemidler.brille.db.VedtakStorePostgres
 import no.nav.hjelpemidler.brille.db.VirksomhetModell
 import no.nav.hjelpemidler.brille.db.VirksomhetStorePostgres
 import no.nav.hjelpemidler.brille.enhetsregisteret.EnhetsregisteretClient
+import no.nav.hjelpemidler.brille.enhetsregisteret.Organisasjonsenhet
 import no.nav.hjelpemidler.brille.enhetsregisteret.Organisasjonsnummer
 import no.nav.hjelpemidler.brille.enhetsregisteret.Postadresse
 import no.nav.hjelpemidler.brille.exceptions.configureStatusPages
@@ -37,6 +38,8 @@ import no.nav.hjelpemidler.brille.kafka.KafkaProducer
 import no.nav.hjelpemidler.brille.medlemskap.MedlemskapBarn
 import no.nav.hjelpemidler.brille.medlemskap.MedlemskapClient
 import no.nav.hjelpemidler.brille.model.AvvisningsType
+import no.nav.hjelpemidler.brille.model.Organisasjon
+import no.nav.hjelpemidler.brille.model.TidligereBrukteOrganisasjonerForOptiker
 import no.nav.hjelpemidler.brille.pdl.PdlClient
 import no.nav.hjelpemidler.brille.pdl.PdlService
 import no.nav.hjelpemidler.brille.redis.RedisClient
@@ -204,7 +207,24 @@ fun Application.setupRoutes() {
 
                 get("/orgnr") {
                     val fnrOptiker = call.extractFnr()
-                    call.respond(vedtakStore.hentTidligereBrukteOrgnrForOptikker(fnrOptiker))
+
+                    val tidligereBrukteOrgnrForOptikker: List<String> =
+                        vedtakStore.hentTidligereBrukteOrgnrForOptikker(fnrOptiker)
+
+                    val organisasjoner: List<Organisasjon> = tidligereBrukteOrgnrForOptikker.map {
+                        val orgEnhet: Organisasjonsenhet =
+                            enhetsregisteretClient.hentOrganisasjonsenhet(Organisasjonsnummer(it))
+                        Organisasjon(
+                            orgnummer = orgEnhet.organisasjonsnummer,
+                            navn = orgEnhet.navn,
+                            adresse = "${orgEnhet.forretningsadresse}, ${orgEnhet.forretningsadresse.postnummer} ${orgEnhet.forretningsadresse.poststed}"
+                        )
+                    }
+                    val response = TidligereBrukteOrganisasjonerForOptiker(
+                        sistBrukteOrganisasjon = organisasjoner.firstOrNull(),
+                        tidligereBrukteOrganisasjoner = organisasjoner
+                    )
+                    call.respond(response)
                 }
 
                 get("/enhetsregisteret/enheter/{organisasjonsnummer}") {
