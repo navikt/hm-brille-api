7d88915e039680a3470935311d9c7e7310e5bc77
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-07-18 13:33:56 +0200

    Logger feil i vilkårsvurdering

--- src/main/kotlin/no/nav/hjelpemidler/brille/vilkarsvurdering/VilkårApi.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/vilkarsvurdering/VilkårApi.kt
@@ -6,6 +6,7 @@ import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
+import mu.KotlinLogging
 import no.nav.hjelpemidler.brille.Configuration
 import no.nav.hjelpemidler.brille.audit.AuditService
 import no.nav.hjelpemidler.brille.extractFnr
@@ -13,35 +14,45 @@ import no.nav.hjelpemidler.brille.nare.evaluering.Resultat
 import no.nav.hjelpemidler.brille.sats.SatsKalkulator
 import no.nav.hjelpemidler.brille.sats.SatsType
 
+
+private val log = KotlinLogging.logger { }
 fun Route.vilkårApi(vilkårsvurderingService: VilkårsvurderingService, auditService: AuditService) {
     post("/vilkarsgrunnlag") {
-        if (Configuration.prod) { // TODO: fjern før prodsetting
-            call.respond(HttpStatusCode.Unauthorized)
-            return@post
-        }
 
-        val vilkårsgrunnlag = call.receive<VilkårsgrunnlagDto>()
-        auditService.lagreOppslag(
-            fnrInnlogget = call.extractFnr(),
-            fnrOppslag = vilkårsgrunnlag.fnrBarn,
-            oppslagBeskrivelse = "[POST] /vilkarsgrunnlag - Sjekk om barn og bestilling oppfyller vilkår for støtte"
-        )
-        val vilkarsvurdering = vilkårsvurderingService.vurderVilkårBrille(vilkårsgrunnlag)
-        val sats = when (vilkarsvurdering.utfall) {
-            Resultat.JA -> SatsKalkulator(vilkårsgrunnlag.brilleseddel).kalkuler()
-            else -> SatsType.INGEN
-        }
+        try {
 
-        val beløp = minOf(vilkårsgrunnlag.brillepris, sats.beløp)
 
-        call.respond(
-            VilkårsvurderingDto(
-                resultat = vilkarsvurdering.utfall,
-                sats = sats,
-                satsBeskrivelse = sats.beskrivelse,
-                satsBeløp = sats.beløp.toString(),
-                beløp = beløp.toString()
+            if (Configuration.prod) { // TODO: fjern før prodsetting
+                call.respond(HttpStatusCode.Unauthorized)
+                return@post
+            }
+
+            val vilkårsgrunnlag = call.receive<VilkårsgrunnlagDto>()
+            auditService.lagreOppslag(
+                fnrInnlogget = call.extractFnr(),
+                fnrOppslag = vilkårsgrunnlag.fnrBarn,
+                oppslagBeskrivelse = "[POST] /vilkarsgrunnlag - Sjekk om barn og bestilling oppfyller vilkår for støtte"
             )
-        )
+            val vilkarsvurdering = vilkårsvurderingService.vurderVilkårBrille(vilkårsgrunnlag)
+            val sats = when (vilkarsvurdering.utfall) {
+                Resultat.JA -> SatsKalkulator(vilkårsgrunnlag.brilleseddel).kalkuler()
+                else -> SatsType.INGEN
+            }
+
+            val beløp = minOf(vilkårsgrunnlag.brillepris, sats.beløp)
+
+            call.respond(
+                VilkårsvurderingDto(
+                    resultat = vilkarsvurdering.utfall,
+                    sats = sats,
+                    satsBeskrivelse = sats.beskrivelse,
+                    satsBeløp = sats.beløp.toString(),
+                    beløp = beløp.toString()
+                )
+            )
+        } catch (e: Exception) {
+            log.error("Feil i vilkårsvurdering", e)
+        }
     }
+
 }
