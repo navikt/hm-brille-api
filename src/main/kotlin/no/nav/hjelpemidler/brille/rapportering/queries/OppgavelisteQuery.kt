57c80af546b6c7476b12a90fb5237b1f1aaca252
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-07-21 12:29:54 +0200

    Retta bug med string concatination i spørring

--- src/main/kotlin/no/nav/hjelpemidler/brille/rapportering/queries/OppgavelisteQuery.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/rapportering/queries/OppgavelisteQuery.kt
@@ -6,17 +6,17 @@ import no.nav.hjelpemidler.brille.store.COLUMN_LABEL_TOTAL
 fun kravlinjeQuery(
     kravFilter: KravFilter? = null
 ): String {
-    val sql = """
+    var sql = """
                 SELECT id, bestillingsdato, behandlingsresultat, opprettet, belop, bestillingsreferanse, count(*) over() AS $COLUMN_LABEL_TOTAL
             FROM vedtak_v1
             WHERE orgnr = :orgNr 
                        """
     if (kravFilter?.equals(KravFilter.EGENDEFINERT) == true) {
-        sql.plus(" AND opprettet >= :fraDato AND opprettet <= :tilDato ")
+        sql = sql.plus(" AND opprettet >= :fraDato AND opprettet <= :tilDato ")
     } else if (kravFilter?.equals(KravFilter.HITTILAR) == true) {
-        sql.plus(" AND date_part('year', opprettet) = date_part('year', CURRENT_DATE)")
+        sql = sql.plus(" AND date_part('year', opprettet) = date_part('year', CURRENT_DATE)")
     } else if (kravFilter?.equals(KravFilter.SISTE3MND) == true) {
-        sql.plus(" AND opprettet >  CURRENT_DATE - INTERVAL '3 months'")
+        sql = sql.plus(" AND opprettet >  CURRENT_DATE - INTERVAL '3 months'")
     }
 
     //language=PostgreSQL
