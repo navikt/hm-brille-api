5ecd18f9778887107d74bc530dc1c7afa945abe4
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-08-26 12:12:16 +0200

    Nyett krav til sletting

---
+++ src/main/resources/db/migration/V11__arkiverKrav.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO vedtak_slettet_v1
+    SELECT id, fnr_barn, fnr_innsender, orgnr, bestillingsdato, brillepris, bestillingsreferanse, vilkarsvurdering, behandlingsresultat, sats, sats_belop, sats_beskrivelse, belop
+    FROM vedtak_v1
+    WHERE id in(16);
+
+DELETE FROM vedtak_v1 WHERE id in(16);
+
