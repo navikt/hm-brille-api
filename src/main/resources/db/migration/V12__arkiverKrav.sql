0fd6b06041a34aaf5f3da0073134046c839e749a
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-08-29 13:52:26 +0200

    11-13 var allerede migrert

--- src/main/resources/db/migration/V12__arkiverKrav.sql
+++ src/main/resources/db/migration/V12__arkiverKrav.sql
@@ -2,7 +2,7 @@
 INSERT INTO vedtak_slettet_v1
     SELECT id, fnr_barn, fnr_innsender, orgnr, bestillingsdato, brillepris, bestillingsreferanse, vilkarsvurdering, behandlingsresultat, sats, sats_belop, sats_beskrivelse, belop
     FROM vedtak_v1
-    WHERE id in(16);
+    WHERE id in(2189,2222);
 
-DELETE FROM vedtak_v1 WHERE id in(16);
+DELETE FROM vedtak_v1 WHERE id in(2189,2222);
 
