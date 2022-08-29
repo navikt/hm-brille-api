a16f1edc8ce94722bd89f4426581694e39371bda
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-08-29 08:55:21 +0200

    Oppdaterte utbetalingsevent

--- src/main/kotlin/no/nav/hjelpemidler/brille/utbetaling/UtbetalingsBatch.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/utbetaling/UtbetalingsBatch.kt
@@ -1,7 +1,10 @@
 package no.nav.hjelpemidler.brille.utbetaling
 
-import java.math.BigDecimal
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageProblems
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
 
 data class UtbetalingsBatch(
     val utbetalinger: List<Utbetaling>,
@@ -14,24 +17,69 @@ data class UtbetalingsBatch(
     }
 }
 
-data class UtbetalingsLinje(val id: Long, val vedtakId: Long, val referanse: String, val belop: BigDecimal)
+data class UtbetalingsLinje(
+    val delytelseId: Long,
+    val endringskode: String,
+    val klassekode: String,
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val sats: Int,
+    val satstype: String,
+    val saksbehandler: String
+)
 
 data class UtbetalingsMelding(
-    val eventName: String = "hm-utbetaling-melding",
+    val eventName: String = "hm-barnebrille-utbetaling",
     val type: String = "Batch",
-    val mottaker: String,
+    val opprettetDato: LocalDateTime,
+    val orgNr: String,
     val batchId: String,
     val utbetalingsdato: LocalDate,
     val utbetalingslinjer: List<UtbetalingsLinje>
-)
+
+) {
+    internal fun toJson(): String {
+        return JsonMessage("{}", MessageProblems("")).also {
+            it["eventName"] = this.eventName
+            it["eventId"] = UUID.randomUUID()
+            it["opprettetDato"] = LocalDateTime.now()
+            it["orgNr"] = this.orgNr
+            it["batchId"] = this.batchId
+            it["Utbetaling"] = Utbetaling(
+                listOf("BARNBRIL"),
+                listOf("NY"),
+                "BB",
+                orgNr,
+                utbetalingslinjer
+            )
+        }.toJson()
+    }
+
+    data class Utbetaling(
+        val fagområde: List<String>,
+        val endringskode: List<String>,
+        val saksbehandler: String,
+        val mottaker: String,
+        val linjer: List<UtbetalingsLinje>
+    )
+}
 
 fun Utbetaling.toUtbetalingsLinje(): UtbetalingsLinje = UtbetalingsLinje(
-    id = id, vedtakId = vedtakId,
-    referanse = vedtak.bestillingsreferanse, belop = vedtak.beløp
+    delytelseId = id,
+    endringskode = "NY",
+    klassekode = "BARNEBRILLER",
+    fom = LocalDate.now(),
+    tom = LocalDate.now(),
+    sats = vedtak.beløp.toInt(),
+    satstype = "ENG",
+    saksbehandler = "BB"
 )
 
 fun UtbetalingsBatch.lagMelding(): UtbetalingsMelding = UtbetalingsMelding(
-    mottaker = orgNr, batchId = batchId, utbetalingsdato = utbetalinger.first().utbetalingsdato,
+    opprettetDato = LocalDateTime.now(),
+    orgNr = orgNr,
+    batchId = batchId,
+    utbetalingsdato = utbetalinger.first().utbetalingsdato,
     utbetalingslinjer = utbetalinger.map { it.toUtbetalingsLinje() }
 )
 
