6db6f4361e3f0277169effa69b09a8bce069c821
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-08-30 15:23:29 +0200

    Make project/region/dbinstance for cronjob dynamic based on env

--- src/main/kotlin/no/nav/hjelpemidler/brille/Configuration.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/Configuration.kt
@@ -23,7 +23,10 @@ object Configuration {
             "ALTINN_APIKEY" to "",
             "ALTINN_APIGW_APIKEY" to "",
             "UTBETALING_ENABLED" to "false",
-            "ELECTOR_PATH" to ""
+            "ELECTOR_PATH" to "",
+            "cronjob.gcp.project" to "",
+            "cronjob.gcp.region" to "",
+            "cronjob.gcp.dbinstance" to "",
         )
     )
 
@@ -71,6 +74,9 @@ object Configuration {
             "enhetsregisteret_base_url" to "http://hm-mocks/brille/enhetsregisteret/api",
             "altinn.altinnUrl" to "https://api-gw-q1.oera.no/ekstern/altinn/api/serviceowner",
             "altinn.proxyConsumerId" to "hjelpemidlerdigitalsoknad-api-dev",
+            "cronjob.gcp.project" to "teamdigihot-dev-9705",
+            "cronjob.gcp.region" to "europe-north1",
+            "cronjob.gcp.dbinstance" to "hm-brille-api-db-dev",
         )
     )
 
@@ -88,6 +94,9 @@ object Configuration {
             "enhetsregisteret_base_url" to "https://data.brreg.no/enhetsregisteret/api",
             "altinn.altinnUrl" to "https://api-gw.oera.no/ekstern/altinn/api/serviceowner",
             "altinn.proxyConsumerId" to "hjelpemidlerdigitalsoknad-api-prod",
+            "cronjob.gcp.project" to "teamdigihot-prod-6f0d",
+            "cronjob.gcp.region" to "europe-north1",
+            "cronjob.gcp.dbinstance" to "hm-brille-api-db-prod",
         )
     )
 
@@ -161,6 +170,10 @@ object Configuration {
         val databasePassword: String = this["DB_PASSWORD"],
         val databaseHost: String = this["DB_HOST"],
         val databasePort: String = this["DB_PORT"],
+
+        val cronjobGcpProject: String = this["cronjob.gcp.project"],
+        val cronjobGcpRegion: String = this["cronjob.gcp.region"],
+        val cronjobGcpDbInstance: String = this["cronjob.gcp.dbinstance"],
     )
 
     data class KafkaProperties(
