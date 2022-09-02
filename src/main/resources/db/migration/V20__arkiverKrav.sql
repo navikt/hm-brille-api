55fe8e520ab4eddc3433cbc0e2d66fdd8dbe8298
Erik Eiesland <erik@eies.land>
2022-09-02 15:52:07 +0200

    sletter vedtak med id 1816

---
+++ src/main/resources/db/migration/V20__arkiverKrav.sql
@@ -0,0 +1,20 @@
+INSERT INTO vedtak_slettet_v1
+SELECT id,
+       fnr_barn,
+       fnr_innsender,
+       orgnr,
+       bestillingsdato,
+       brillepris,
+       bestillingsreferanse,
+       vilkarsvurdering,
+       behandlingsresultat,
+       sats,
+       sats_belop,
+       sats_beskrivelse,
+       belop
+FROM vedtak_v1
+WHERE id IN (1816);
+
+DELETE
+FROM vedtak_v1
+WHERE id IN (1816);
