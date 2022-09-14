76be76e32b10ac79bf6fef7cb133004450390a1d
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-09-14 10:34:23 +0200

    Arkiver krav 3260

---
+++ src/main/resources/db/migration/V24__arkiverKrav.sql
@@ -0,0 +1,21 @@
+INSERT INTO vedtak_slettet_v1
+SELECT id,
+       fnr_barn,
+       fnr_innsender,
+       orgnr,
+       bestillingsdato,
+       brillepris,
+       bestillingsreferanse,
+       vilkarsvurdering,
+       behandlingsresultat,
+       sats,
+       sats_belop,
+       sats_beskrivelse,
+       belop,
+       opprettet
+FROM vedtak_v1
+WHERE id IN (3260);
+
+DELETE
+FROM vedtak_v1
+WHERE id IN (3260);
