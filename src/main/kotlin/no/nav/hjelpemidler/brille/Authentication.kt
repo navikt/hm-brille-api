ef6593c922e5ecea898fff7972dbbd16d0ab4171
Erik Eiesland <erik@eies.land>
2023-03-12 16:22:21 +0100

    bytter oppslag mot well-known med miljøvariabler

--- src/main/kotlin/no/nav/hjelpemidler/brille/Authentication.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/Authentication.kt
@@ -2,18 +2,15 @@ package no.nav.hjelpemidler.brille
 
 import com.auth0.jwk.JwkProviderBuilder
 import com.fasterxml.jackson.annotation.JsonProperty
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.auth.Principal
 import io.ktor.server.auth.authentication
 import io.ktor.server.auth.jwt.jwt
 import io.ktor.server.auth.principal
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
+import no.nav.hjelpemidler.http.openid.AzureADEnvironmentVariable
+import no.nav.hjelpemidler.http.openid.TokenXEnvironmentVariable
 import java.net.URL
 import java.util.UUID
 import java.util.concurrent.TimeUnit
@@ -23,31 +20,15 @@ const val AZURE_AD_AUTH = "azureAd"
 
 private val log = KotlinLogging.logger {}
 
-fun Application.installAuthentication(httpClient: HttpClient) {
-    var tokenXConfig: AuthenticationConfiguration
-    runBlocking(Dispatchers.IO) {
-        tokenXConfig = AuthenticationConfiguration(
-            metadata = httpClient.get(Configuration.tokenXProperties.wellKnownUrl).body(),
-            clientId = Configuration.tokenXProperties.clientId,
-        )
-    }
-
-    val jwkProviderTokenx = JwkProviderBuilder(URL(tokenXConfig.metadata.jwksUri))
+fun Application.installAuthentication() {
+    val jwkProviderTokenx = JwkProviderBuilder(URL(TokenXEnvironmentVariable.TOKEN_X_JWKS_URI))
         // cache up to 1000 JWKs for 24 hours
         .cached(1000, 24, TimeUnit.HOURS)
         // if not cached, only allow max 100 different keys per minute to be fetched from external provider
         .rateLimited(100, 1, TimeUnit.MINUTES)
         .build()
 
-    var azureAdConfig: AuthenticationConfiguration
-    runBlocking(Dispatchers.IO) {
-        azureAdConfig = AuthenticationConfiguration(
-            metadata = httpClient.get(Configuration.azureAdProperties.wellKnownUrl).body(),
-            clientId = Configuration.azureAdProperties.clientId,
-        )
-    }
-
-    val jwkProviderAzureAd = JwkProviderBuilder(URL(azureAdConfig.metadata.jwksUri))
+    val jwkProviderAzureAd = JwkProviderBuilder(URL(AzureADEnvironmentVariable.AZURE_OPENID_CONFIG_JWKS_URI))
         // cache up to 1000 JWKs for 24 hours
         .cached(1000, 24, TimeUnit.HOURS)
         // if not cached, only allow max 100 different keys per minute to be fetched from external provider
@@ -56,12 +37,12 @@ fun Application.installAuthentication(httpClient: HttpClient) {
 
     authentication {
         jwt(TOKEN_X_AUTH) {
-            verifier(jwkProviderTokenx, tokenXConfig.metadata.issuer)
+            verifier(jwkProviderTokenx, TokenXEnvironmentVariable.TOKEN_X_ISSUER)
             validate { credentials ->
                 requireNotNull(credentials.payload.audience) {
                     "Auth: Missing audience in token"
                 }
-                require(credentials.payload.audience.contains(tokenXConfig.clientId)) {
+                require(credentials.payload.audience.contains(TokenXEnvironmentVariable.TOKEN_X_CLIENT_ID)) {
                     "Auth: Valid audience not found in claims"
                 }
                 require(credentials.payload.getClaim("acr").asString() == ("Level4")) { "Auth: Level4 required" }
@@ -74,16 +55,17 @@ fun Application.installAuthentication(httpClient: HttpClient) {
             }
         }
         jwt(AZURE_AD_AUTH) {
-            verifier(jwkProviderAzureAd, azureAdConfig.metadata.issuer)
+            verifier(jwkProviderAzureAd, AzureADEnvironmentVariable.AZURE_OPENID_CONFIG_ISSUER)
             validate { credentials ->
                 requireNotNull(credentials.payload.audience) {
                     "Auth: Missing audience in token"
                 }
-                require(credentials.payload.audience.contains(azureAdConfig.clientId)) {
+                require(credentials.payload.audience.contains(AzureADEnvironmentVariable.AZURE_APP_CLIENT_ID)) {
                     "Auth: Valid audience not found in claims"
                 }
                 UserPrincipalAdmin(
-                    credentials.payload.getClaim("oid").asString()?.let { oid -> kotlin.runCatching { UUID.fromString(oid) }.getOrNull() },
+                    credentials.payload.getClaim("oid").asString()
+                        ?.let { oid -> kotlin.runCatching { UUID.fromString(oid) }.getOrNull() },
                     credentials.payload.getClaim("preferred_username").asString(),
                     credentials.payload.getClaim("name").asString()
                 )
@@ -91,7 +73,13 @@ fun Application.installAuthentication(httpClient: HttpClient) {
         }
         provider("local_azuread") {
             authenticate { context ->
-                context.principal(UserPrincipalAdmin(UUID.fromString("21547b88-65da-49bf-8117-075fb40e6682"), "example@example.com", "Example some some"))
+                context.principal(
+                    UserPrincipalAdmin(
+                        UUID.fromString("21547b88-65da-49bf-8117-075fb40e6682"),
+                        "example@example.com",
+                        "Example some some"
+                    )
+                )
             }
         }
     }
@@ -119,7 +107,8 @@ fun ApplicationCall.extractFnr(): String {
     return fnrFromClaims
 }
 
-internal class UserPrincipalAdmin(private val oid: UUID?, private val email: String?, private val name: String?) : Principal {
+internal class UserPrincipalAdmin(private val oid: UUID?, private val email: String?, private val name: String?) :
+    Principal {
     fun getUUID() = oid
     fun getEmail() = email
     fun getName() = name
