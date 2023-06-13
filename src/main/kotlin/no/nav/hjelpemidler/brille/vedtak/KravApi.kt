94975a8c9e7f4cd12c93bc93d697943b4092ef8a
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2023-06-13 16:30:07 +0200

    Revert "Test 500 error ved krav innsending /krav"
    This reverts commit d302d5267bce7a797c6e51bdc80f56549cbf4bb6.

--- src/main/kotlin/no/nav/hjelpemidler/brille/vedtak/KravApi.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/vedtak/KravApi.kt
@@ -25,8 +25,6 @@ internal fun Route.kravApi(
 ) {
     route("/krav") {
         post {
-            // FIXME: TEST - Kristoffer
-            return@post call.respond(HttpStatusCode.InternalServerError, "Nope")
             val kravDto = call.receive<KravDto>()
             val fnrInnsender = call.extractFnr()
             val navnInnsender = redisClient.optikerNavn(fnrInnsender) ?: "<Ukjent>"
