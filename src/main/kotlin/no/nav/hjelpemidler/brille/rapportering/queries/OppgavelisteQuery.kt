d74b6b6afce8ad1e19d6ad15a970e2b0d599c5ae
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2023-08-17 17:00:41 +0200

    Skriv om database spørring for rapporten til hente et git antall utbetalinger per side, ikke et gitt antall vedtak

--- src/main/kotlin/no/nav/hjelpemidler/brille/rapportering/queries/OppgavelisteQuery.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/rapportering/queries/OppgavelisteQuery.kt
@@ -10,61 +10,73 @@ fun kravlinjeQuery(
     referanseFilter: String?,
 ): String {
     var sql = """
-        SELECT
-            COALESCE(v.id, vs.id) AS id,
-            COALESCE(v.bestillingsdato, vs.bestillingsdato) AS bestillingsdato,
-            COALESCE(v.behandlingsresultat, vs.behandlingsresultat) AS behandlingsresultat,
-            COALESCE(v.opprettet, vs.opprettet) AS opprettet,
-            COALESCE(v.belop, vs.belop) AS belop,
-            COALESCE(v.bestillingsreferanse, vs.bestillingsreferanse) AS bestillingsreferanse,
-            COALESCE(u1.utbetalingsdato, u2.utbetalingsdato) AS utbetalingsdato,
-            COALESCE(u1.batch_id, u2.batch_id) AS batch_id,
-            vs.slettet,
-            count(*) over() AS $COLUMN_LABEL_TOTAL
-        FROM vedtak_v1 v
-        FULL OUTER JOIN vedtak_slettet_v1 vs ON v.id = vs.id
-        LEFT JOIN utbetaling_v1 u1 ON v.id = u1.vedtak_id
-        LEFT JOIN utbetaling_v1 u2 ON vs.id = u2.vedtak_id
-        WHERE
-            (v.orgnr = :orgNr OR vs.orgnr = :orgNr)
-            -- Bare inkluder resultater fra slettet-vedtak tabellen som ble utbetalt før de ble slettet:
-            AND (vs.id IS NULL OR u2.utbetalingsdato IS NOT NULL)
-    """
+        WITH alle_vedtak AS (
+        	-- Slå sammen vanlige og slettede vedtak
+        	SELECT
+        		COALESCE(v.id, vs.id) AS id,
+        		COALESCE(v.fnr_barn, vs.fnr_barn) AS fnr_barn,
+        		COALESCE(v.fnr_innsender, vs.fnr_innsender) AS fnr_innsender,
+        		COALESCE(v.orgnr, vs.orgnr) AS orgnr,
+        		COALESCE(v.bestillingsdato, vs.bestillingsdato) AS bestillingsdato,
+        		COALESCE(v.brillepris, vs.brillepris) AS brillepris,
+        		COALESCE(v.bestillingsreferanse, vs.bestillingsreferanse) AS bestillingsreferanse,
+        		COALESCE(v.vilkarsvurdering, vs.vilkarsvurdering) AS vilkarsvurdering,
+        		COALESCE(v.behandlingsresultat, vs.behandlingsresultat) AS behandlingsresultat,
+        		COALESCE(v.sats, vs.sats) AS sats,
+        		COALESCE(v.sats_belop, vs.sats_belop) AS sats_belop,
+        		COALESCE(v.sats_beskrivelse, vs.sats_beskrivelse) AS sats_beskrivelse,
+        		COALESCE(v.belop, vs.belop) AS belop,
+        		COALESCE(v.opprettet, vs.opprettet) AS opprettet,
+        		COALESCE(v.navn_innsender, vs.navn_innsender) AS navn_innsender,
+        		COALESCE(v.kilde, vs.kilde) AS kilde,
+        		vs.slettet,
+        		vs.slettet_av_type
+        	FROM vedtak_v1 v
+        	FULL OUTER JOIN vedtak_slettet_v1 vs ON v.id = vs.id
+        	ORDER BY COALESCE(v.id, vs.id) DESC
+        ), grupperte_resultater AS (
+        	-- Grupper vedtak sammen basert på avstemmingsreferanse eller dag de ble
+        	-- opprettet hvis de ikke har fått avstemmingsreferanse enda. Ekskluder
+        	-- slettede vedtak som ble slettet før utbetaling.
+        	SELECT u.batch_id, DATE(v.opprettet), array_agg(v.id) AS vedtak_ids, count(*) over() AS $COLUMN_LABEL_TOTAL
+        	FROM alle_vedtak v
+        	LEFT JOIN utbetaling_v1 u ON v.id = u.vedtak_id
+        	WHERE v.orgnr = :orgNr AND (v.slettet IS NULL OR u.id IS NOT NULL)
+        	GROUP BY DATE(v.opprettet), u.batch_id
+        	ORDER BY DATE(v.opprettet) DESC
+        ), grupperte_resultater_pagination AS (
+        	-- Paginer resultatene og gi oss lister med vedtak-ider for hvert resultat
+        	SELECT batch_id, vedtak_ids, pagination_total FROM grupperte_resultater LIMIT :limit OFFSET :offset
+        )
+        -- Ekspander de paginerte resultatene igjen til alle relevante vedtak
+        SELECT * FROM grupperte_resultater_pagination grp
+        LEFT JOIN alle_vedtak v ON v.id = ANY(grp.vedtak_ids)
+
+        WHERE TRUE
+    """.trimIndent()
 
     if (tilDato != null && kravFilter?.equals(KravFilter.EGENDEFINERT) == true) {
         sql = sql.plus(
             """
-            AND (
-                (v.id IS NULL OR (v.opprettet >= :fraDato AND v.opprettet <= :tilDato))
-                AND (vs.id IS NULL OR (vs.opprettet >= :fraDato AND vs.opprettet <= :tilDato))
-            )
-            """.trimMargin()
+                AND v.opprettet >= :fraDato AND v.opprettet <= :tilDato
+            """.trimIndent()
         )
     } else if (tilDato == null && kravFilter?.equals(KravFilter.EGENDEFINERT) == true) {
         sql = sql.plus(
             """
-            AND (
-                (v.id IS NULL OR (v.opprettet >= :fraDato))
-                AND (vs.id IS NULL OR (vs.opprettet >= :fraDato))
-            )
+                AND v.opprettet >= :fraDato
             """.trimIndent()
         )
     } else if (kravFilter?.equals(KravFilter.HITTILAR) == true) {
         sql = sql.plus(
             """
-            AND (
-                (v.id IS NULL OR (date_part('year', v.opprettet) = date_part('year', CURRENT_DATE)))
-                AND (vs.id IS NULL OR (date_part('year', vs.opprettet) = date_part('year', CURRENT_DATE)))
-            )
+                AND date_part('year', v.opprettet) = date_part('year', CURRENT_DATE)
             """.trimIndent()
         )
     } else if (kravFilter?.equals(KravFilter.SISTE3MND) == true) {
         sql = sql.plus(
             """
-            AND (
-                (v.id IS NULL OR (v.opprettet >  CURRENT_DATE - INTERVAL '3 months'))
-                AND (vs.id IS NULL OR (vs.opprettet >  CURRENT_DATE - INTERVAL '3 months'))
-            )
+                AND v.opprettet > CURRENT_DATE - INTERVAL '3 months'
             """.trimIndent()
         )
     }
@@ -72,24 +84,14 @@ fun kravlinjeQuery(
     if (!referanseFilter.isNullOrBlank()) {
         sql = sql.plus(
             """
-             AND (
-                CAST(v.id AS TEXT) LIKE :referanseFilter
-                OR v.bestillingsreferanse LIKE :referanseFilter
-                OR u1.batch_id LIKE :referanseFilter
-                
-                OR CAST(vs.id AS TEXT) LIKE :referanseFilter
-                OR vs.bestillingsreferanse LIKE :referanseFilter
-                OR u2.batch_id LIKE :referanseFilter
-              )
+                AND (
+                	CAST(v.id AS TEXT) LIKE :referanseFilter
+                	OR v.bestillingsreferanse LIKE :referanseFilter
+                	OR grp.batch_id LIKE :referanseFilter
+                )
             """.trimIndent()
         )
     }
-
-    sql = sql.plus(
-        """
-            ORDER BY opprettet DESC
-        """.trimIndent()
-    )
 
     //language=PostgreSQL
     return sql.trimIndent()
