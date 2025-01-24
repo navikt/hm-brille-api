c30f7cfa704b152285720d952fa186e37cf3c3f9
Erik Eiesland <erik.eiesland@nav.no>
2025-01-24 15:31:18 +0100

    gradle 8.12.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:25.024.094300")
+            from("no.nav.hjelpemidler:katalog:25.024.145609")
             library("hmRapidsAndRiversV2Core", "com.github.navikt:hm-rapids-and-rivers-v2-core:202410290928")
             version("spotless", "6.25.0")
         }
