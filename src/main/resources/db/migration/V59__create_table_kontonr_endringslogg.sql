48983ce5aa49c3787224a7e39e371014deff08bb
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-01-30 13:55:24 +0100

    Fiks sql feil
    Co-authored-by: erlingjd <erling.johannes.danbolt@nav.no>

--- src/main/resources/db/migration/V59__create_table_kontonr_endringslogg.sql
+++ src/main/resources/db/migration/V59__create_table_kontonr_endringslogg.sql
@@ -4,15 +4,15 @@ CREATE TABLE IF NOT EXISTS kontonr_endringslogg_v1
     orgnr             ORGNR     NOT NULL REFERENCES virksomhet_v1 (orgnr),
     fnr_oppdatert_av  FNR       NOT NULL,
     kontonr           KONTONR   NOT NULL,
-    opprettet         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    opprettet         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Legg inn en første rad for dagens avtaler hvor man ikke har hatt endringer....
-INSERT INTO (orgnr, fnr_oppdatert_av, kontonr, opprettet) SELECT orgnr, fnr_innsender, kontonr, opprettet FROM virksomhet_v1 WHERE opprettet = oppdatert;
+INSERT INTO kontonr_endringslogg_v1 (orgnr, fnr_oppdatert_av, kontonr, opprettet) SELECT orgnr, fnr_innsender, kontonr, opprettet FROM virksomhet_v1 WHERE opprettet = oppdatert;
 
 -- Setter et utgangspunkt for virksomheter som har vært endret siden opprettelse, og lagrer hva kontonr har vært siden da,
 -- men vi kan ikke være sikre på at det var kontonr og ikke e-postadressen som ble endret den dagen. Vi vet heller ikke
 -- historien til kontonr'et før den datoen.
-INSERT INTO (orgnr, fnr_oppdatert_av, kontonr, opprettet) SELECT orgnr, fnr_oppdatert_av, kontonr, oppdatert FROM virksomhet_v1 WHERE opprettet <> oppdatert;
+INSERT INTO kontonr_endringslogg_v1 (orgnr, fnr_oppdatert_av, kontonr, opprettet) SELECT orgnr, fnr_oppdatert_av, kontonr, oppdatert FROM virksomhet_v1 WHERE opprettet <> oppdatert;
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
