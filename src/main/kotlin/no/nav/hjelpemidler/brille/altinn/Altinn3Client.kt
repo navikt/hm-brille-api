25a07249daf4603d6b41936245c4a8e1c91f56e4
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-04-03 10:53:42 +0200

    Les ut Avgivere fra nytt api

--- src/main/kotlin/no/nav/hjelpemidler/brille/altinn/Altinn3Client.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/altinn/Altinn3Client.kt
@@ -1,14 +1,15 @@
 package no.nav.hjelpemidler.brille.altinn
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.client.call.body
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.accept
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
-import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.headers
+import kotlinx.serialization.json.JsonArray
 import no.nav.hjelpemidler.brille.Configuration
 import no.nav.hjelpemidler.configuration.MaskinportenEnvironmentVariable
 import no.nav.hjelpemidler.http.correlationId
@@ -29,6 +30,26 @@ class Altinn3Client {
         }
     }
 
+    enum class ResponseType(val type: String) {
+        Person("Person"),
+        Organization("Organization"),
+    }
+
+    data class ResponseObject(
+        val partyUuid: String,
+        val partyId: Long,
+        val type: String,
+        val unitType: String,
+        val personId: String?,
+        val organizationNumber: String?,
+        val name: String,
+        val isDeleted: Boolean,
+        val onlyHierarchyElementWithNoAccess: Boolean,
+        val authorizedResources: JsonArray,
+        val authorizedRoles: List<String>,
+        val subunits: List<ResponseObject>,
+    )
+
     suspend fun test(fnr: String) {
         data class Reqeust(
             val type: String = "urn:altinn:person:identifier-no",
@@ -36,12 +57,33 @@ class Altinn3Client {
         )
 
         log.info { "Requesting resources for $fnr" }
-        val result = client.post("/accessmanagement/api/v1/resourceowner/authorizedparties?includeAltinn2=true") {
+        val response = client.post("/accessmanagement/api/v1/resourceowner/authorizedparties?includeAltinn2=true") {
             setBody(Reqeust(value = fnr))
         }
-        log.info { "Result: ${result.status}" }
+        log.info { "Result: ${response.status}" }
 
-        val body = result.bodyAsText()
-        log.info { "Body: $body" }
+        val result = response.body<List<ResponseObject>>()
+        log.info { "Debug result: $result" }
+
+        val orgs = result.filter {
+            // Bare se på organisasjoner
+            runCatching { ResponseType.valueOf(it.type) }.getOrNull() == ResponseType.Organization
+        }.flatMap {
+            // Pakk ut underenheter ala. gammel implementasjon
+            listOf(
+                *it.subunits.map { innerIt -> Pair(innerIt, it.organizationNumber) }.toTypedArray(),
+                Pair(it.copy(subunits = emptyList()), null),
+            )
+        }
+        log.info { "Organisasjoner: $orgs" }
+
+        val avgivere = orgs.map {
+            Avgiver(
+                navn = it.first.name,
+                orgnr = it.first.partyUuid,
+                parentOrgnr = it.second,
+            )
+        }
+        log.info { "Avgivere: $avgivere" }
     }
 }
