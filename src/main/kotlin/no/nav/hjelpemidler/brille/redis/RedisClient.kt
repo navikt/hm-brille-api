9d4592f3cb106e6d76760a3c147a60ea165ebc61
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-08-12 13:02:22 +0200

    Slå på ssl for jedis/valkey

--- src/main/kotlin/no/nav/hjelpemidler/brille/redis/RedisClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/brille/redis/RedisClient.kt
@@ -21,6 +21,7 @@ class RedisClient(private val redisProps: Configuration.RedisProperties = Config
             2 * 1000, // 2 seconds
             redisProps.username,
             redisProps.password,
+            true,
         )
     }
 
